6afe39d7252c4069974f74fea13e90151cfbe1e2
Charles Mungai <charles.mbuthia19@gmail.com>
2023-01-10 13:51:17 +0300

    Initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x3314f6fcf3580FaaF95312266Fd0eB1EB5AcBE49";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
---
+++ my-app/constants/index.js
@@ -0,0 +1,9 @@
+
+import { abi as cryptodev} from '../contracts/CryptoDevs.json'
+import { abi as cryptodevtoken} from '../contracts/CryptoDevToken.json'
+
+
+export const NFT_CONTRACT_ABI = cryptodev;
+export const NFT_CONTRACT_ADDRESS = "0x3314f6fcf3580FaaF95312266Fd0eB1EB5AcBE49";
+export const TOKEN_CONTRACT_ABI = cryptodevtoken;
+export const TOKEN_CONTRACT_ADDRESS = "0x7173C511864c2D0d0335227405A8bD7B81ddaDB9";
